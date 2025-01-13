b504b956adc61c53c631af613af4e06c21b22ea0
Thakshila navodya watadenithanna <65487716+Thakshila-N@users.noreply.github.com>
2025-01-13 21:12:41 +0530

    initial commit

---
+++ src/main/java/com/departmentcrud/deparmentcrud/DeparmentcrudApplication.java
@@ -0,0 +1,13 @@
+package com.departmentcrud.deparmentcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeparmentcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeparmentcrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/departmentcrud/deparmentcrud/controller/Department.java
@@ -0,0 +1,12 @@
+package com.departmentcrud.deparmentcrud.controller;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Department {
+	
+	private long depId;
+	private String depName;
+
+
+}
